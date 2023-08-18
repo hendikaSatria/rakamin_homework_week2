fd0aa32f44d4c3ce28f679f742c50e4834a982f7
hendika <casuale.production@gmail.com>
2023-08-18 23:17:56 +0700

    Final BANGET

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const btnNav =  document.querySelector(".btn-mobile-nav");
 const header =  document.querySelector(".header");
+const btnReadMore =  document.getElementById("btn-read-more");
 
 btnNav.addEventListener('click', function() {
     header.classList.toggle("open");
 });
+
+
